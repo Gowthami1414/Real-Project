80ef3055865e82d0b5a68cab9455419155172770
Gowthami1414 <156208887+Gowthami1414@users.noreply.github.com>
2024-03-02 09:57:51 +0530

    Navbar.js

--- Navbar.js
+++ Navbar.js
@@ -1,29 +1,41 @@
- import React from 'react'
- import a from "../images/Alarm.png";
- import b from"../images/Male User.png";
- import c from"../images/Communicate.png";
- import {Link} from 'react-router-dom';
- import '../App.css';
+import React, { useState } from 'react';
+import {Link} from 'react-router-dom';
+import ProfileContainer from '../components/Profile/ProfileContainer';
 
- 
- const Navbar = () => {
-   return (
-     <div className="navbar" >
-      <div class=" border-2 border-[#FF858D] py-2 px-4 mx-56 my-4 rounded-full items-center"> 
-      <ul className="flex flex-row text-[#F71735] items-clenter justify-around  ">
-      <Link to="/dashboard"><li  >Dashboard</li></Link>
-      <Link to="/hr"><li  >HR</li></Link>
-      <Link to="/time"><li  >Time</li></Link>
-      <Link to="/leave"><li  > Leave</li></Link>
-      <Link to="/profile"><li  >Profile</li></Link>
-      
-      <li> <img src={b} alt ='Male user' style={{height:'75%' ,width:'50%'}}></img> </li>
-      <li><img src= {c} alt='communicate' style={{height:'75%' ,width:'50%'}}></img></li>
-      <li><img src= {a} alt ='c' style={{height:'75%' ,width:'50%'}}></img> </li></ul> 
-      </div>
-      </div>
+import a from "../components/Profile/images/Alarm.png";
+import b from"../components/Profile/images/Male User.png";
+import c from"../components/Profile/images/Communicate.png";
 
-   )
- }
- 
- export default Navbar
+import '../App.css';
+
+
+const Navbar = () => {
+  const [showProfileContainer, setShowProfileContainer] = useState(false);
+
+  // eslint-disable-next-line
+  const handleProfileClick = () => {
+    setShowProfileContainer(!showProfileContainer);
+  };
+
+  return (
+    <div className="navbar">
+      <div className="border-2 border-[#FF858D] py-2 px-4 mx-56 my-4 rounded-full items-center">
+        <ul className="flex flex-row text-[#F71735] items-center justify-around">
+          <Link to=""><li>Dashboard</li></Link>
+          <Link to="/hr"><li>HR</li></Link>
+          <Link to="/time"><li>Time</li></Link>
+          <Link to="/leave"><li>Leave</li></Link>
+          <li onClick={handleProfileClick}>Profile</li>
+
+          <li> <img src={b} alt ='Male user' style={{height:'75%' ,width:'50%'}}></img> </li>
+          <li><img src= {c} alt='communicate' style={{height:'75%' ,width:'50%'}}></img></li>
+          <li><img src= {a} alt ='c' style={{height:'75%' ,width:'50%'}}></img> </li>
+          {/* Add other icons */}
+        </ul>
+      </div>
+      {showProfileContainer && <ProfileContainer />}
+    </div>
+  );
+}
+
+export default Navbar;
