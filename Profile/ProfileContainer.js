b0b8e6d1bd89afc2d8e8696d3f5320d775cca0df
Gowthami1414 <156208887+Gowthami1414@users.noreply.github.com>
2024-03-02 19:34:11 +0530

    ProfileContainer.js

--- Profile/ProfileContainer.js
+++ Profile/ProfileContainer.js
@@ -9,31 +9,31 @@ import per from '../../components/Profile/images/Growth.png';
 import pri from '../../components/Profile/images/Prize.png';
 const ProfileContainer = () => {
   return (
-    <div className="profile-container" style={{ position: 'absolute', top: '60px', left: 0, height: '500px', width: '250px' }}>
+    <div className="profile-container" style={{ position: 'absolute', top: '60px', left: 0, height: '555px', width: '250px' }}>
       <img src={youngManImage} alt="Young Man Holding Laptop" style={{ width: '400px', alignItems: 'center', height: '200px' }} />
       <div style={{ marginTop: '8px', display: 'inline-flex', alignItems: 'center' }}>
         <img src={userImage} alt="User" style={{ width: '50px', height: '50px', marginRight: '10px' }} />
-        <span>My Profile</span>
+        <Link to=""><p>My Profile</p></Link>
       </div>
       <div style={{ marginTop: '8px',display: 'inline-flex', alignItems: 'center'}}>
         <img src={familyImage} alt="Family" style={{ width: '40px', height: '40px', marginRight: '20px' }} />
-        <Link to="/familydetails"><li>Family Details</li></Link>
+        <Link to="/ProfileForm"><p>Family Details</p></Link>
       </div>
       <div style={{ marginTop: '8px',display: 'inline-flex', alignItems: 'center'}}>
         <img src={employeeImage} alt="Family" style={{ width: '40px', height: '40px', marginRight: '20px' }} />
-        <span>Employee Info</span>
+        <Link to="" ><p>Employee Info</p></Link>
       </div>
       <div style={{ marginTop: '8px',display: 'inline-flex', alignItems: 'center'}}>
         <img src={com} alt="Family" style={{ width: '40px', height: '40px', marginRight: '20px' }} />
-        <span>Compensation</span>
+        <Link to=""><p>Compensation</p></Link>
       </div>
       <div style={{ marginTop: '8px',display: 'inline-flex', alignItems: 'center'}}>
         <img src={per} alt="Family" style={{ width: '40px', height: '40px', marginRight: '20px' }} />
-        <span>Performance</span>
+        <Link to=""><p>Performance</p></Link>
       </div>
       <div style={{ marginTop: '8px',display: 'inline-flex', alignItems: 'center'}}>
         <img src={pri} alt="Family" style={{ width: '40px', height: '40px', marginRight: '20px' }} />
-        <span>Rewards</span>
+        <Link to=""><p>Rewards</p></Link>
       </div>
     </div>
   );
