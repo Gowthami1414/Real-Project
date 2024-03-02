9f0d66f77dcfdfee8295c36dbb9363a0368c0485
Gowthami1414 <156208887+Gowthami1414@users.noreply.github.com>
2024-03-02 19:39:28 +0530

    App.js

--- App.js
+++ App.js
@@ -3,20 +3,30 @@ import Navbar from "./components/Navbar";
 import{BrowserRouter as Router,Route } from 'react-router-dom';
 import Dashboard from "./components/Dashboard";
 import ProfileContainer from './components/Profile/ProfileContainer';
+import ProfileForm from './components/Profile/ProfileForm';
+
+
+
 
 
 const App = () => {
   return (
-    <Router>
+    <div className='bg-[#E3F2FD]'>
+    <Router className='bg-[#E3F2FD]'>
       <div className="app-container">
-        <ProfileContainer />
-        <div className="content-container"></div>
-        <Navbar />
-          <Route path="/Dashboard" component={Dashboard} />
-          <Route path="/profile" component={ProfileContainer} /> {/* Define the route for the profile page */}
+        <div className="left-container">  
+  
+        </div>
+        <div className='bg-[#E3F2FD]'></div>
+          <Navbar />
+          <Route path="/Dashboard" component={Dashboard} className='bg-[#E3F2FD]'/>
+          <Route path="/profile" component={ProfileContainer} className='bg-[#E3F2FD]' />
+          <Route path="/ProfileForm" component={ProfileForm} className='bg-[#E3F2FD]'/>
+           {/* Define the route for the profile page */}
           {/* Add routes for other components/pages here */}
       </div>
     </Router>
+    </div>
   );
 };
 
